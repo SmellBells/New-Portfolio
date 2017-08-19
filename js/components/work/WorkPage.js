9ff8ffcfa7fe2b3b0558a7bde2fe71a486e2476b
SmellBells <elliott.gonzales@gmail.com>
2017-08-18 21:42:59 -0600

    added a background color to work for testing purposes

--- js/components/work/WorkPage.js
+++ js/components/work/WorkPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 class WorkPage extends React.Component {
     render() {
         return (
-            <div>
+            <div className="otherPagescontainer">
                 <h1>My projects will be on this page</h1>
             </div>
         );
